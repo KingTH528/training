18ed8b4a7658266d09f07b0c96d8f41b3aad5dc9
KingTH528 <spm528k@gmail.com>
2023-08-08 18:31:59 +0900

    ㅁ

--- src/algorithm_ex/DfsSoldier.java
+++ src/algorithm_ex/DfsSoldier.java
@@ -2,24 +2,32 @@ package algorithm_ex;
 
 import java.util.Scanner;
 
+
+//자기꺼 상하좌우 확인하면서 진행
+//새로운 정보 부터 우선 탐색
+
 public class DfsSoldier {
+	
+	//W, B 조심할려고 이넘 사용
 	enum SoldierColor {
 	    WHITE('W'), BLUE('B');
-
-	    char codeA;
-
-	    SoldierColor(char codeA) {
-	        this.codeA = codeA;
+		char codeA;
+		SoldierColor(char codeA) {
+			this.codeA = codeA;
 	    }
-
+	    public static SoldierColor fromChar(char c) {
+	        for (SoldierColor color : values()) {
+	        	if (color.codeA == c) return color;
+	        }
+	        throw new IllegalArgumentException("No matching enum found for character: " + c);
+	    }
+	    
 	}
-
-
 	private int N, M;
-	private char[][] field;
+	private SoldierColor[][] field;
 	private boolean[][] visited;
 
-	public DfsSoldier(int N, int M, char[][] field) {
+	public DfsSoldier(int N, int M, SoldierColor[][] field) {
 		this.N = N;
 		this.M = M;
 		this.field = field;
@@ -36,11 +44,12 @@ public class DfsSoldier {
 
 				// 이번게 방문 안한거면
 				if (!visited[i][j]) {
+					
 					// size변수 하나 만들어서 상하좌후 재귀적으로 보고 사이즈 더하는 함수 호출(안에서 방문한건지 한번더 확인하니까 신경끄고)
 					int size = dfs(i, j, field[i][j]);
 					
 					
-					if (field[i][j] == SoldierColor.WHITE.codeA) {
+					if (field[i][j] == SoldierColor.WHITE) {
 						whitePower += size * size;
 					} else {
 						bluePower += size * size;
@@ -51,7 +60,7 @@ public class DfsSoldier {
 		return new int[] { whitePower, bluePower };
 	}
 
-	private int dfs(int x, int y, char currentColor) {
+	private int dfs(int x, int y, SoldierColor currentColor) {
 
 		// x,y가 각각 0~M-1,N-1일때 / 이미 확인한거 / W,B 아닌거 이 세종류 제외
 		if (x < 0 || x >= M || y < 0 || y >= N || visited[x][y] || field[x][y] != currentColor) {
@@ -82,12 +91,12 @@ public class DfsSoldier {
 		try (Scanner scanner = new Scanner(System.in)) {
 			int N = scanner.nextInt();
 			int M = scanner.nextInt();
-			char[][] field = new char[M][N];
+			SoldierColor[][] field = new SoldierColor[M][N];
 
 			for (int i = 0; i < M; i++) {
 				String line = scanner.next();
 				for (int j = 0; j < N; j++) {
-					field[i][j] = line.charAt(j);
+					field[i][j] = SoldierColor.fromChar(line.charAt(j));
 				}
 			}
 			long startTime = System.currentTimeMillis();
@@ -102,6 +111,7 @@ public class DfsSoldier {
 			long timeElapsed = endTime - startTime;
 			System.out.println("Execution time in milliseconds: " + timeElapsed);
 		}
+		
 		// 메모리 사용량 측정
 		Runtime runtime = Runtime.getRuntime();
 		long memory = runtime.totalMemory() - runtime.freeMemory();
--- src/algorithm_ex/enumTest.java
+++ src/algorithm_ex/enumTest.java
@@ -14,7 +14,11 @@ public class enumTest {
 	        this.str = str;
 	    }
 	    
-	    public static String getName(char codeA) {
+	    public void setStr(String str) {
+			this.str = str;
+		}
+
+		public static String getName(char codeA) {
 	        for (enumSample sample : values()) {
 	            if (sample.codeA == codeA) {
 	                return sample.name();
@@ -41,22 +45,39 @@ public class enumTest {
 	    	return null;
 	    }
 	    
+	    
+	    //toString 오버라이드해서 str리턴하게 바꿈
+	    @Override
+	    public String toString() {
+	        return this.str;
+	    }
+	    
+//	    //이름을 string값으로 리턴
 //	    @Override
 //	    public String toString() {
-//	        return this.name();
+//	    	return this.name();
 //	    }
 	}
 	
 	public static void main(String[] args) {
 		
+//		String testChar = enumSample.WHITE.toString();
+//		System.out.println(  testChar );
 		System.out.println(enumSample.WHITE);
 		System.out.println(enumSample.WHITE.codeA);
+		System.out.println();
 		System.out.println(enumSample.WHITE.codeB);
 		System.out.println(enumSample.WHITE.str);
 		System.out.println();
 		System.out.println(enumSample.getName('W'));
 		System.out.println(enumSample.getName(0));
 		System.out.println(enumSample.getName("codeW"));
+		System.out.println(enumSample.WHITE.ordinal());
+		
+		enumSample.WHITE.setStr("testtesttest");
+		
+		System.out.println(enumSample.WHITE);
+		
 	}
 
 }
