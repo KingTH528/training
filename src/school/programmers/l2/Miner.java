1fb635481684d813bb13ed7510666d5a8dc2a8ec
KingTH528 <spm528k@gmail.com>
2023-08-14 13:39:25 +0900

    a

--- src/school/programmers/l2/Miner.java
+++ src/school/programmers/l2/Miner.java
@@ -1,29 +1,28 @@
 package school.programmers.l2;
 
+import java.util.Arrays;
+
 public class Miner {
 
 	public static void main(String[] args) {
+//		int[] picks = {1, 3, 2};
+//		String[] minerals = {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"};
+		int[] picks = {0, 1, 1};
+		String[] minerals = {"diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond"};
 		
-		
-		
-		
-		
-		
+		System.out.println("\n답 : " + solution(picks, minerals));
 
 	}
 
-	
-	
-	public int solution(int[] picks, String[] minerals) {
+	public static int solution(int[] picks, String[] minerals) {
 		int answer = 0;
-
 
 		// 미네랄 -> 곡갱이별 피로도로 정리
 		int row = minerals.length/5+1;
 		if(minerals.length%5==0){
 			row -= 1;
 		}
-		
+		System.out.println("row : " + row);
 		int[][] mineralsInt = new int[row][3];
 		int mineralsD = 0;
 		int mineralsI = 0;
@@ -37,7 +36,12 @@ public class Miner {
 			else {};
 			
 			//5의 배수면 줄바꿈 및 피로도 정리
-			if(i!=0 && i%5==0) {
+			if((i!=0 && i%5==4) || i == minerals.length-1) {
+				System.out.println();
+				System.out.println("==줄바꿈==(" + thisRow + ")->" + (thisRow+1));
+				System.out.println("다이아 수 : " + mineralsD);
+				System.out.println("철 수 : " + mineralsI);
+				System.out.println("돌 수 : " + mineralsS);
 				
 				//곡갱이별 피로도 정리
 				//다이아 곡갱이 썼을 때
@@ -47,26 +51,72 @@ public class Miner {
 				//돌 곡갱이 썼을 때
 				mineralsInt[thisRow][2] = 25*mineralsD + 5*mineralsI + mineralsS;
 				
-				//피로도 초기화
 				mineralsD = 0;
 				mineralsI = 0;
 				mineralsS = 0;
 				
-				//줄바꿈
 				thisRow++; 
 			}
-			
-			
-			//이걸 곡갱이별 피로도 말고 곡갱이별 피로도의 차이를 구하고 그 차이가 큰것 부터 다이아 -> 철 -> 돌 순서로 넣으면?
-			// 다<->철, 철<->돌 피로도 차이 구하고 차이가 큰 걸 
-			
-			
+		}
+
+		
+		System.out.println("\n피로도 정리");
+		for(int[] i : mineralsInt) {
+			for(int j : i) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
 		}
 		
 		
 		
+		int tempAnswer = 0;
+		int tempIndex = -1;
 		
 		
+		for(int i = 0 ; i < picks[0] ; i++) {
+			for(int j = 0 ; j<row ; j++) {
+				int thisD = (mineralsInt[j][1] -  mineralsInt[j][0]);
+				if(tempAnswer < thisD) {
+					tempAnswer = thisD;
+					tempIndex = j;
+				}else {
+					
+				}
+			}
+			answer += mineralsInt[tempIndex][0];
+			mineralsInt[tempIndex][0] = 0;
+			mineralsInt[tempIndex][1] = 0;
+			mineralsInt[tempIndex][2] = 0;
+			tempAnswer = 0;
+		}
+		
+		for(int i = 0 ; i < picks[1] ; i++) {
+			for(int j = 0 ; j<row ; j++) {
+				int thisD = (mineralsInt[j][2] -  mineralsInt[j][1]);
+				if(tempAnswer < thisD) {
+					tempAnswer = thisD;
+					tempIndex = j;
+				}
+			}
+			answer += mineralsInt[tempIndex][1];
+			mineralsInt[tempIndex][0] = 0;
+			mineralsInt[tempIndex][1] = 0;
+			mineralsInt[tempIndex][2] = 0;
+			tempAnswer = 0;
+		}
+		
+		for(int i = 0 ; i < picks[2] ; i++) {
+			for(int j = 0 ; j<row ; j++) {
+				if(mineralsInt[j][2] != 0) tempIndex = j;
+			}
+			//여기
+			answer += mineralsInt[tempIndex][2];
+			mineralsInt[tempIndex][0] = 0;
+			mineralsInt[tempIndex][1] = 0;
+			mineralsInt[tempIndex][2] = 0;
+		}
+
 		return answer;
 	}
 }
