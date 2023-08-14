23883e822bf66e7b89c402165cd3c693d4bfdbec
KingTH528 <spm528k@gmail.com>
2023-08-14 09:59:53 +0900

    ㅁ

--- src/algorithm_ex/BfsSoldier.java
+++ src/algorithm_ex/BfsSoldier.java
@@ -5,86 +5,116 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BfsSoldier {
-	private int N, M;
-	private char[][] field;
-	private boolean[][] visited;
-	private final int[] dx = { -1, 1, 0, 0 };
-	private final int[] dy = { 0, 0, -1, 1 };
 
-	public BfsSoldier(int N, int M, char[][] field) {
-		this.N = N;
-		this.M = M;
-		this.field = field;
-		this.visited = new boolean[M][N];
-	}
+    enum SoldierColor {
+        WHITE('W'), BLUE('B');
+        char codeA;
 
-	private int bfs(int x, int y, char currentColor) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] { x, y });
-		visited[x][y] = true;
+        SoldierColor(char codeA) {
+            this.codeA = codeA;
+        }
 
-		int size = 0;
+        public static SoldierColor fromChar(char c) {
+            for (SoldierColor color : values()) {
+                if (color.codeA == c) return color;
+            }
+            throw new IllegalArgumentException("No matching enum found for character: " + c);
+        }
+    }
 
-		while (!queue.isEmpty()) {
-			int[] current = queue.poll();
-			size++;
+    private static class Position {
+        int x, y;
 
-			for (int i = 0; i < 4; i++) {
-				int newX = current[0] + dx[i];
-				int newY = current[1] + dy[i];
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-				if (newX >= 0 && newX < M && newY >= 0 && newY < N && !visited[newX][newY]
-						&& field[newX][newY] == currentColor) {
-					queue.offer(new int[] { newX, newY });
-					visited[newX][newY] = true;
-				}
-			}
-		}
+    private int N, M;
+    private SoldierColor[][] field;
+    private boolean[][] visited;
 
-		return size;
-	}
+    public BfsSoldier(int N, int M, SoldierColor[][] field) {
+        this.N = N;
+        this.M = M;
+        this.field = field;
+        this.visited = new boolean[M][N];
+    }
 
-	public int[] calculatePower() {
-		int whitePower = 0;
-		int bluePower = 0;
+    public int[] calculatePower() {
+        int whitePower = 0;
+        int bluePower = 0;
 
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					int size = bfs(i, j, field[i][j]);
-					if (field[i][j] == 'W') {
-						whitePower += size * size;
-					} else {
-						bluePower += size * size;
-					}
-				}
-			}
-		}
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    int size = bfs(i, j);
+                    if (field[i][j] == SoldierColor.WHITE) {
+                        whitePower += size * size;
+                    } else {
+                        bluePower += size * size;
+                    }
+                }
+            }
+        }
+        return new int[] { whitePower, bluePower };
+    }
 
-		return new int[] { whitePower, bluePower };
-	}
+    private int bfs(int x, int y) {
+        int size = 0;
 
-	public static void main(String[] args) {
+        int[] dx = { 0, 0, -1, 1 };
+        int[] dy = { 1, -1, 0, 0 };
+
+        SoldierColor currentColor = field[x][y];
+        Queue<Position> queue = new LinkedList<>();
+        queue.add(new Position(x, y));
+
+        while (!queue.isEmpty()) {
+            Position current = queue.poll();
+            if (current.x < 0 || current.x >= M || current.y < 0 || current.y >= N || visited[current.x][current.y] || field[current.x][current.y] != currentColor) {
+                continue;
+            }
+
+            visited[current.x][current.y] = true;
+            size++;
+
+            for (int i = 0; i < 4; i++) {
+                int newX = current.x + dx[i];
+                int newY = current.y + dy[i];
+                queue.add(new Position(newX, newY));
+            }
+        }
+
+        return size;
+    }
+
+    public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-			int N = scanner.nextInt();
-			int M = scanner.nextInt();
-			char[][] field = new char[M][N];
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+            SoldierColor[][] field = new SoldierColor[M][N];
 
-			for (int i = 0; i < M; i++) {
-			    String line = scanner.next();
-			    for (int j = 0; j < N; j++) {
-			        field[i][j] = line.charAt(j);
-			    }
-			}
-			
-			BfsSoldier battleFieldBFS = new BfsSoldier(N, M, field);
-			int[] result = battleFieldBFS.calculatePower();
-			System.out.println(result[0] + " " + result[1]);
-			
-	        // 메모리 사용량 측정
-	        Runtime runtime = Runtime.getRuntime();
-	        long memory = runtime.totalMemory() - runtime.freeMemory();
-	        System.out.println("Used memory in bytes: " + memory);
-		}
-	}
+            for (int i = 0; i < M; i++) {
+                String line = scanner.next();
+                for (int j = 0; j < N; j++) {
+                    field[i][j] = SoldierColor.fromChar(line.charAt(j));
+                }
+            }
+
+            long startTime = System.currentTimeMillis();
+            BfsSoldier battleField = new BfsSoldier(N, M, field);
+            int[] result = battleField.calculatePower();
+            System.out.println(result[0] + " " + result[1]);
+
+            long endTime = System.currentTimeMillis();
+            long timeElapsed = endTime - startTime;
+            System.out.println("Execution time in milliseconds: " + timeElapsed);
+        }
+
+        Runtime runtime = Runtime.getRuntime();
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory in bytes: " + memory);
+    }
 }
--- src/algorithm_ex/DfsSoldier.java
+++ src/algorithm_ex/DfsSoldier.java
@@ -26,22 +26,22 @@ public class DfsSoldier {
 	private int N, M;
 	private SoldierColor[][] field;
 	private boolean[][] visited;
-
+	
 	public DfsSoldier(int N, int M, SoldierColor[][] field) {
 		this.N = N;
 		this.M = M;
 		this.field = field;
 		this.visited = new boolean[M][N];
 	}
-
+	
 	// 힘 계산
 	public int[] calculatePower() {
 		int whitePower = 0;
 		int bluePower = 0;
-
+		
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
-
+				
 				// 이번게 방문 안한거면
 				if (!visited[i][j]) {
 					
@@ -59,32 +59,32 @@ public class DfsSoldier {
 		}
 		return new int[] { whitePower, bluePower };
 	}
-
+	
 	private int dfs(int x, int y, SoldierColor currentColor) {
-
+		
 		// x,y가 각각 0~M-1,N-1일때 / 이미 확인한거 / W,B 아닌거 이 세종류 제외
 		if (x < 0 || x >= M || y < 0 || y >= N || visited[x][y] || field[x][y] != currentColor) {
 			return 0;
 		}
-
+		
 		visited[x][y] = true;
 		int size = 1;
-
+		
 		int[] dx = { 0, 0, -1, 1 };
 		int[] dy = { 1, -1, 0, 0 };
-
+		
 		// 상하좌우 확인, 대각선 무시
 		for (int i = 0; i < 4; i++) {
 			int newX = x + dx[i];
 			int newY = y + dy[i];
-
+			
 			// 재귀적으로 상하좌우 확인하면서 size더하기(0일때랑 M,N일때는 안쪽에서 return 0)
 			size += dfs(newX, newY, currentColor);
 		}
-
+		
 		return size;
 	}
-
+	
 	public static void main(String[] args) {
 
 		// 입력값 초기화 하고
@@ -92,7 +92,7 @@ public class DfsSoldier {
 			int N = scanner.nextInt();
 			int M = scanner.nextInt();
 			SoldierColor[][] field = new SoldierColor[M][N];
-
+			
 			for (int i = 0; i < M; i++) {
 				String line = scanner.next();
 				for (int j = 0; j < N; j++) {
@@ -101,12 +101,12 @@ public class DfsSoldier {
 			}
 			long startTime = System.currentTimeMillis();
 			DfsSoldier battleField = new DfsSoldier(N, M, field);
-
+			
 			// 결과 계산
 			int[] result = battleField.calculatePower();
-
+			
 			System.out.println(result[0] + " " + result[1]);
-
+			
 			long endTime = System.currentTimeMillis();
 			long timeElapsed = endTime - startTime;
 			System.out.println("Execution time in milliseconds: " + timeElapsed);
