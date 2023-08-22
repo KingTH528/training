746a8217e0ee3cfba5d005ef062fea29d7a6bb9e
KingTH528 <spm528k@gmail.com>
2023-08-22 09:24:00 +0900

    a

--- src/school/programmers/l1/personalinfo/Tester.java
+++ src/school/programmers/l1/personalinfo/Tester.java
@@ -7,16 +7,16 @@ import java.util.Map;
 
 public class Tester {
 	public static void main(String[] args) {
-//		String today = "2022.05.19";
-//		String[] terms = {"A 6", "B 12", "C 3"};
-//		String[] privacies = {"2021.05.02 A", 
-//							  "2021.07.01 B", 
-//							  "2022.02.19 C", 
-//							  "2022.02.20 C"};
+		String today = "2022.05.19";
+		String[] terms = {"A 6", "B 12", "C 3"};
+		String[] privacies = {"2021.05.02 A", 
+							  "2021.07.01 B", 
+							  "2022.02.19 C", 
+							  "2022.02.20 C"};
 		
-		String today = "2020.01.01";
-		String[] terms = {"Z 3", "D 5"};
-		String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+//		String today = "2020.01.01";
+//		String[] terms = {"Z 3", "D 5"};
+//		String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
 		
 		int[] sdvf = new Solution().solution(today, terms, privacies);
 
@@ -38,7 +38,6 @@ class Solution {
         	String[] tempStr = str.split(" ");
         	policy.put(tempStr[0], Integer.valueOf(tempStr[1]) );
         }
-        
         for(String str : privacies) {
         	Map<String, Object> info = new HashMap<String, Object>();
         	String[] infoStr = str.split(" ");
@@ -50,10 +49,13 @@ class Solution {
         //privacies마다 terms로 만료일 확인 후 오늘 날짜랑 비교
         List<Integer> tempAnswer = new ArrayList<Integer>();
         for(int i = 0 ; i < regList.size() ; i++) {
-        	Integer[] tempDate = prev( (Integer[])(regList.get(i).get("regDate")), policy.get(regList.get(i).get("policy")));
+        	Integer[] tempDate = prev(
+        							  (Integer[])(regList.get(i).get("regDate")), 
+        							   policy.get(regList.get(i).get("policy"))  );
         	if(compare(todayArr, tempDate)) {
         		tempAnswer.add(i);
         	}
+        	
         }
         
         //배열로 정리
@@ -61,7 +63,6 @@ class Solution {
         for(int i = 0 ; i< answer.length ; i++) {
         	answer[i] = tempAnswer.get(i)+1;
         }
-        
         return answer;
     }
     
@@ -99,7 +100,7 @@ class Solution {
     	
     	if(now[0]>reg[0]) {
     		return true;
-    	}else if(now[0]>reg[0]){
+    	}else if(now[0]<reg[0]){
     		return false;
     	}else if(now[1]>reg[1]) {
     		return true;
