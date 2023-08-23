6ca47e6f0975e62953249b48490a90980babcd3d
KingTH528 <spm528k@gmail.com>
2023-08-23 17:28:43 +0900

    a

--- src/school/programmers/l1/mbti/Tester.java
+++ src/school/programmers/l1/mbti/Tester.java
@@ -1,8 +1,12 @@
 package school.programmers.l1.mbti;
 
+import java.util.*;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/118666
 public class Tester {
 	public static void main(String[] args) {
+		
+		//survey의 원소는 "RT", "TR" / "FC", "CF" / "MJ", "JM" / "AN", "NA" 중 하나
 		
 		// 결과 : "TCMA"
 		String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
@@ -23,12 +27,35 @@ public class Tester {
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
+        int[] pointInt  = { 0,  0,  0,  0,  0,  0,  0,  0};
+        String pointStr = "RTCFJMAN";
         
+        for(int i = 0 ; i < choices.length ; i++) {
+        	if(choices[i] < 4) {
+        		char tempChar = survey[i].charAt(1);
+        		int index =  pointStr.indexOf(tempChar);
+        		pointInt[index] += choices[i]-4;
+        	}else {
+        		char tempChar = survey[i].charAt(0);
+        		int index =  pointStr.indexOf(tempChar);
+        		pointInt[index] += 4-choices[i];
+        	}
+        }
         
+        char[] ch = new char[4];
+        for(int i = 0 ; i < 8 ; i += 2) {
+        	if(pointInt[i]<pointInt[i+1]) {
+        		ch[i/2] = pointStr.charAt(i+1);
+        	}else {
+        		ch[i/2] = pointStr.charAt(i);
+        	}
+        	
+        }
         
-        
-        return answer;
+        return new String(ch);
     }
 }
+
+
+
 
