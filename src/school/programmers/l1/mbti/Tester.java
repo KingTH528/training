ca58e1c0e5ce2400a48353c5d91858acd0a2d16b
KingTH528 <spm528k@gmail.com>
2023-08-23 16:41:09 +0900

    a

---
+++ src/school/programmers/l1/mbti/Tester.java
@@ -0,0 +1,34 @@
+package school.programmers.l1.mbti;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/118666
+public class Tester {
+	public static void main(String[] args) {
+		
+		// 결과 : "TCMA"
+		String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+		int [] choices = {5, 3, 2, 7, 5};
+		
+//		// 결과 : "RCJA"
+//		String[] survey = {"TR", "RT", "TR"};
+//		int [] choices = {7, 1, 3};
+		
+		
+		String answer = new Solution().solution(survey, choices);
+		System.out.println("\n========== 답 ==========\n" + answer);
+		
+	}
+}
+
+
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        
+        
+        
+        
+        return answer;
+    }
+}
+
