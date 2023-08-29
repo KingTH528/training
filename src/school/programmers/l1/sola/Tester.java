0be36b8bbf8a138f17d96a252ccea662643c9055
KingTH528 <spm528k@gmail.com>
2023-08-29 13:17:39 +0900

    a

--- src/school/programmers/l1/sola/Tester.java
+++ src/school/programmers/l1/sola/Tester.java
@@ -1,23 +1,17 @@
 package school.programmers.l1.sola;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-//처음에 
+import java.util.*;
 
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/86052
 public class Tester {
 	public static void main(String[] args) {
 		
-		// 결과 : 16
-		String[] grid = {"SL","LR"};
+//		// 결과 : 16
+//		String[] grid = {"SL","LR"};
 		
-//		// 결과 : 1,1,1,1	
-//		String[] grid = {"S"};
+		// 결과 : 1,1,1,1	
+		String[] grid = {"S"};
 		
 //		// 결과 : 4,4
 //		String[] grid = {"R","R"};
@@ -42,110 +36,170 @@ public class Tester {
 	// 2. 각 빛(프로세스) 별 정보 -> 처음 위치, 처음 방향
 	
 	
-class Solution {
-    public int[] solution(String[] grid) {
-    	List<Integer> cycleList = new ArrayList<Integer>();
-    	
-    	// 방향 정리  ->  s만나면 direction += 0;  /  r만나면 direction += 1; (4 -> 0)  /  l만나면 direction += -1; (-1 -> 3) 
-    	int direction = -1;
-    	int[] dX = {0,1,0,-1};
-    	int[] dY = {1,0,-1,0};
-    	int cycleLength = 0;
-    	
-    	// 현재 위치 저장
-    	int[] xy = {0,0};
-    	
-    	// 노드별 정보 정리
-    	int[][] nodeInfo = new int[grid[0].length()][grid.length];
-    	boolean[][][] check = new boolean[grid[0].length()][grid.length][4];  // true면 이미 지나감
-    	
-    	for(int i = 0 ; i < grid[0].length() ; i++) {
-    		for(int j = 0 ; j < grid.length ; j++) {
-    			if(grid[i].charAt(j)=='S')      nodeInfo[i][j] = 0;
-    			else if(grid[i].charAt(j)=='R') nodeInfo[i][j] = 1;
-    			else if(grid[i].charAt(j)=='L') nodeInfo[i][j] = -1;
-    		}
-    	}
-    	
-    	
-    	
-    	
-    	// 1. 처음 들어오는 빛 정보 저장(x, y, 방향)
-    	// 사이클 길이 0부터 시작(들어올때 +1) 임시로 cycleLength 표현
-    	xy[0] = 0;
-    	xy[1] = 0;
-    	direction = 2;
-    	int[] startInfo = {xy[0], xy[1], direction};  // 나중에 사이클 판단할때 사용
-    	
-    	
-    	//2. 이전에 쓴적없는 거면 이번에 꺾는 방향 적용
-    	if(!check[xy[0]] [xy[1]] [direction]) {
-    		// 이번 칸의 nodeInfo값dmf 방향에 적용
-    		direction += nodeInfo[xy[0]][xy[1]];
-    		if(direction < 0) direction = 3;
-    		else if(direction > 3) direction = 0;
-    	}else {
-    		
-    	} 
-    	
-    	//3. 해당 방향으로 이동
-    	xy[0] += dX[direction];
-    	xy[1] += dX[direction];
-    	if(xy[0] < 0) xy[0] = nodeInfo[0].length;
-    	else if(xy[1] < 0) xy[1] = nodeInfo.length;
-    	else if(xy[0] > nodeInfo[0].length) xy[0] = 0;
-    	else if(xy[1] > nodeInfo.length) xy[1] = 0;
-    	
-    	//4. 이동 직후 cycleLength++;
-    	cycleLength++; //j로 대체 가능
-    	
-    	
-    	
-    	//===================================================================//
-    	
-    	// 이전에 쓴적없는 거면 이번에 꺾는 방향 적용
-    	if(!check[xy[0]] [xy[1]] [direction]) {
-    		// 이번 칸의 nodeInfo값dmf 방향에 적용
-    		direction += nodeInfo[xy[0]][xy[1]];
-    		if(direction < 0) direction = 3;
-    		else if(direction > 3) direction = 0;
-    		
-    	// 이전에 쓴 적이 있으면 무조건 중복 또는 싸이클 완성!!
-    	}else {
-    		// startInfo랑 같으면 사이클 하나 생김 (int(사이클 길이) 추가해야됨)
-    		if( startInfo[0] == xy[0] &&
-    			startInfo[1] == xy[1] &&
-    			startInfo[2] == direction) {
-    			cycleList.add(cycleLength);
-    		}
-    	}
-    	
+//class Solution {
+//	int[][] nodeInfo;
+//	boolean[][][] check;
+//	int[] dX = {0,1,0,-1};
+//	int[] dY = {1,0,-1,0};
+//	
+//    public int[] solution(String[] grid) {
+//    	List<Integer> cycleList = new ArrayList<Integer>();
+//    	
+//    	// 방향 정리  ->  s만나면 direction += 0;  /  r만나면 direction += 1; (4 -> 0)  /  l만나면 direction += -1; (-1 -> 3) 
+//    	
+//    	// 현재 위치 저장
+//    	int[] xy = {0,0};
+//    	
+//    	// 노드별 정보 정리
+//    	nodeInfo = new int[grid[0].length()][grid.length];            // [x][y]
+//    	check = new boolean[grid[0].length()][grid.length][4];  // true면 이미 지나감
+//    	
+//    	for(int i = 0 ; i < grid[0].length() ; i++) {
+//    		for(int j = 0 ; j < grid.length ; j++) {
+//    			if(grid[j].charAt(i)=='S')      nodeInfo[i][j] = 0;
+//    			else if(grid[j].charAt(i)=='R') nodeInfo[i][j] = 1;
+//    			else if(grid[j].charAt(i)=='L') nodeInfo[i][j] = -1;
+//    		}
+//    	}
+//
+//    	
+//    	for(int i = 0 ; i < nodeInfo.length ; i ++) {
+//    		for(int j = 0 ; j < nodeInfo[0].length ; j ++) {
+//    			for(int k = 0 ; k < 4 ; k++) {
+//    				int startDir = k;
+//    				int[] startXY = {i,j};
+//    				
+//    				int nowLength = cycle(startXY, startDir, startXY, startDir, true)-1;
+//    				if(nowLength>0) {
+//    					cycleList.add(nowLength);
+//    				}
+//    			}
+//    		}
+//    	}
+//    	
+//        int[] answer = new int[cycleList.size()];
+//        for(int i=0 ; i < answer.length ; i++) {
+//        	answer[i] = cycleList.get(i);
+//        }
+//        Arrays.sort(answer);
+//        
+//        return answer;
+//    }
+//    
+//    
+//    public int cycle(int[]startLoc, int startDir, int[]loc, int dir, boolean b) {
+//    	int length = 1;
+//    	// 이전에 지나간 적이 있고
+//    	if(check[loc[0]] [loc[1]] [dir]) {
+//    		// start정보랑 같으면 길이 리턴
+//    		if(startLoc[0]==loc[0] && 
+//			   startLoc[1]==loc[1] && 
+//			   startDir==dir &&
+//			   !b) {
+//    			return length;
+//    		// start정보랑 다르면 0
+//    		}else {
+//    			return 0;
+//    		}
+//    	// 지나간 적이 없으면 다음 방향&위치 구하고
+//    	}else {
+//    		check[loc[0]] [loc[1]] [dir] = true;
+//    		int newDir = turnner(dir, nodeInfo[loc[0]][loc[1]]);
+//    		int[] newLoc = new int[2];
+//    		
+//    		newLoc[0] = loc[0]+dX[newDir];
+//    		if(newLoc[0]<0) {
+//    			newLoc[0]=nodeInfo.length-1;
+//    		}else if(newLoc[0]>nodeInfo.length-1){
+//    			newLoc[0]=0;
+//    		}
+//    		
+//    		newLoc[1] = loc[1]+dY[newDir];
+//    		if(newLoc[1]<0) {
+//    			newLoc[1]=nodeInfo[0].length-1;
+//    		}else if(newLoc[1]>nodeInfo[0].length-1){
+//    			newLoc[1]=0;
+//    		}
+//    		// length를 아래부터 위로 올라가면서 더함(근데 0부터 올라올땐 끝까지 0이 나올 수 있게 해줘야함)
+//    		length *= cycle(startLoc, startDir, newLoc, newDir, false);
+//    		length++;
+//    	}
+//    	
+//    	return length;
+//    }
+//    
+//    
+//    public int turnner(int d, int route) {
+//    		d += route;
+//    		if(d<0) d+=4;
+//    		else if(d>3) d-=4;
+//    	return d;
+//    }
+//    
+//}
 
-    	// 해당 방향으로 이동
-    	xy[0] += dX[direction];
-    	xy[1] += dX[direction];
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-        int[] answer = new int[cycleList.size()];
-        for(int i=0 ; i < answer.length ; i++) {
-        	answer[i] = cycleList.get(i);
+class Solution {
+    int[][] nodeInfo;
+    boolean[][][] check;
+    int[] dX = {0, 1, 0, -1};
+    int[] dY = {1, 0, -1, 0};
+
+    public int[] solution(String[] grid) {
+        List<Integer> cycleList = new ArrayList<>();
+
+        int rows = grid.length;
+        int cols = grid[0].length();
+        nodeInfo = new int[rows][cols];
+        check = new boolean[rows][cols][4];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                char ch = grid[i].charAt(j);
+                nodeInfo[i][j] = ch == 'S' ? 0 : ch == 'R' ? 1 : -1;
+            }
         }
-        
-        
+
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < cols; y++) {
+                for (int dir = 0; dir < 4; dir++) {
+                    if (!check[x][y][dir]) {
+                        Stack<int[]> stack = new Stack<>();
+                        int length = 0;
+
+                        stack.push(new int[]{x, y, dir});
+                        while (!stack.isEmpty()) {
+                            int[] top = stack.pop();
+                            int curX = top[0], curY = top[1], curDir = top[2];
+
+                            if (check[curX][curY][curDir]) {
+                                continue;
+                            }
+
+                            check[curX][curY][curDir] = true;
+                            length++;
+
+                            int newDir = (curDir + nodeInfo[curX][curY] + 4) % 4;
+                            int newX = (curX + dX[newDir] + rows) % rows;
+                            int newY = (curY + dY[newDir] + cols) % cols;
+
+                            stack.push(new int[]{newX, newY, newDir});
+                        }
+
+                        if (length > 0) {
+                            cycleList.add(length);
+                        }
+                    }
+                }
+            }
+        }
+
+        int[] answer = new int[cycleList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = cycleList.get(i);
+        }
+        Arrays.sort(answer);
+
         return answer;
     }
 }
-
-
 
---
+++ src/school/programmers/l1/sola/sev.java
@@ -0,0 +1,21 @@
+package school.programmers.l1.sola;
+
+public class sev {
+
+	public static void main(String[] args) {
+
+		
+		Boolean[][] b = new Boolean[3][2];
+		
+		
+		for(Boolean[] bb : b) {
+			for(Boolean bbb : bb) {
+				System.out.print(bbb + " ");
+				
+			}System.out.println();
+		}
+		
+		
+	}
+
+}
