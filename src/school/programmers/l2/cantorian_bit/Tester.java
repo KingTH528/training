240276706b19b4c234496a5d7f785e6196cc9885
KingTH528 <spm528k@gmail.com>
2023-08-31 17:51:21 +0900

    a

--- src/school/programmers/l2/cantorian_bit/Tester.java
+++ src/school/programmers/l2/cantorian_bit/Tester.java
@@ -16,7 +16,7 @@ public class Tester {
 		// 110  11, 11011, 00000, 11  011, 11011
 		// 123  45, 67890, 12345, 67  890, 12345
 		long n = 2;
-		long l = 4;
+		long l = 6;
 		long r = 17;
 		
 		long answer = new Solution().solution(n, l ,r);
@@ -34,48 +34,67 @@ class Solution {
     // 첫번째칸 ~ a번째칸 까지의 1의 갯수( 갯수 = e-s+1 )
     public long calculator(long a) {
     	System.out.println("-----------");
-    	// int[][] nArray = new long [21][2];
-    	// 같은 수준 두번이면 이후 5^n칸은 전부 0!!!!
-    	// 만약 이후 5^n칸이 a를 넘거나 a면 완전 스탑!!!
-    	// 안넘으면 다음 1부터 시작!!!
     	//
     	// 만약 세번이면 1갯수 안세고 다음칸
     	
     	int[] bit = {1,1,0,1,1};
     	long answer = 0;
-    	long nowIndex = 1;
-        long preN = 1;
-        long newN = -1;
+    	int preIndex = -1;
+    	int nowIndex = 1;
+        int preN = 1;
+        int newN = -1;
         long cntN = 0;
+        int eZero = -1;
     	
         // 갯수기준으로 a 까지의 5의 거듭제곱(갯수)중 가장 큰 값 ( 갯수가 5미만이 될 때까지만 )
-        while( nowIndex < a-5 ) {
+        while( nowIndex < a-3 ) {
         	
-        	long lengt = (int) (a - nowIndex +1);
-        	
+        	// 이번 가장 앞그룹의 수준 확인
         	for(int i = 1 ; Math.pow(5, i) < a-nowIndex ; i++) newN = i;
-        	System.out.println("preN: " + preN + " / newN: " + newN);
-        	if(preN == newN) cntN++;
-        	else cntN = 0;
+        	if(preIndex == nowIndex) break;
         	
-        	nowIndex += Math.pow(5, newN);  // 현재 위치 갱신
+        	// 이전/현재 위치 갱신
+        	preIndex = nowIndex;
+        	nowIndex += Math.pow(5, newN);
+        	System.out.println("preIndex: " + preIndex + " / nowIndex: " + nowIndex + " / cntN: " + cntN);
         	
-        	// 만약 조상이 0이아니면 (= 이번이 세번째 같은 newN이 아니면)
-        	// 
-        	System.out.println("cntN : " + cntN);
-        	if(cntN!=3) {
-        		answer +=  Math.pow(4, newN);        // 1갯수 추가
+        	
+        	// 이전 수준이 2번 같았으면 앞으로 0이 계속나옴 (이전 위치부터A+5^이전수준 까지)
+        	if(cntN == 2) {
+        		// 0이나오는 마지막 위치 저장
+        		eZero = (int) (preIndex + Math.pow(5, preN))-1;
+        		System.out.println("-->eZero: " + eZero);
+        		
+        		// 0영역이 a를 벗어나면 종료 || 0영역 안에서 수준이 낮아지면 종료
+        		if(eZero > a || preN != newN) break;
         	}
-        	preN = newN;                         // 현재 수준 -> 이전 수준으로 변경
         	
+        	// 이전 수준이랑 같으면 ++ 아니면 0
+        	if(preN == newN) cntN++;
+        	else cntN=0;
+
+        	// 이전에 같은게 0영역 아니면 1갯수 추가
+        	if(cntN!=3) {
+        		answer +=  Math.pow(4, newN);
+        	}
         	
+        	if(cntN==10) {
+        		break;
+        	}
+        	System.out.println("temp_answer: " + answer + "(eZero: " + eZero + ")");
         	
+        	// 이번수준 정보 저장
+        	preN = newN;
         }
         
-        for(int i = 0 ; i <= a-nowIndex ; i++) {
-        	answer += bit[i];
+        
+        if(cntN!=2) {
+        	for(int i = 0 ; i <= a-nowIndex ; i++) {
+        		answer += bit[i];
+        	}
+        	
         }
-        System.out.println(answer);
+        System.out.println("answer: " + answer);
     	return answer;
     }
 }
